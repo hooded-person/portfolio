e95dc026eb73d4ceec4558b8074d97e8c5da1d78
Niels Groen <hooded.main@gmail.com>
2025-01-23 21:10:49 +0100

    made dark mode persistent

--- themes.js
+++ themes.js
@@ -1,6 +1,9 @@
 const checkbox = document.getElementById("darkModeCheckbox")
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+let preferedColorScheme =()=>(window && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? "dark" : "light"
+
+// check prefered and previous settings
+if (preferedColorScheme() == "dark" || localStorage.getItem("theme") == "dark") {
     // dark mode
     checkbox.checked = true
 }
@@ -10,6 +13,13 @@ document.body.setAttribute("data-theme", checkbox.checked ? "dark" : "light")
 
 checkbox.addEventListener("change", (e)=>{
     console.log("darkmode was toggled")
+
+    let newTheme = e.target.checked ? "dark" : "light"
     
-    document.body.setAttribute("data-theme", e.target.checked ? "dark" : "light")
+    document.body.setAttribute("data-theme", newTheme)
+    if (preferedColorScheme() != newTheme) {
+        localStorage.setItem("theme", newTheme)
+    } else {
+        localStorage.removeItem("theme")
+    }
 })
